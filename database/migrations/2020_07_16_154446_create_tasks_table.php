e8d45cae4eaf95b81e40566e8c4babab295590f4
Youssef Assouli <assouli.ysf@gmail.com>
2020-08-03 13:48:34 +0100

    auth
    auth spec

---
+++ database/migrations/2020_07_16_154446_create_tasks_table.php
@@ -0,0 +1,32 @@
+<?php
+use IlluminateSupportFacadesSchema;
+use IlluminateDatabaseSchemaBlueprint;
+use IlluminateDatabaseMigrationsMigration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
